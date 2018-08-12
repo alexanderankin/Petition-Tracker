079874e97c07e92097d181abc198467eded676de
Dave Ankin <alexanderankin@gmail.com>
2018-08-12 18:00:23 -0400

    signed cookies

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(process.env['cookie_secret'] || 'keyboard cat'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
