05ef83cd3651c4d178dcb543b75475493f9bf854
Dave Ankin <alexanderankin@gmail.com>
2018-08-12 18:08:24 -0400

    cleaned up authentication code

--- routes/index.js
+++ routes/index.js
@@ -9,36 +9,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/petition/login', function(req, res, next) {
-  if (req.cookies.user) {
-    return res.redirect('/petition');
-  }
-  res.render('login', {});
+  if (req.cookies.user) return res.redirect('/petition');
+  res.render('login');
 });
 
-router.post('/petition/login', function (req, res, next) {
-  console.log(req.body);
+router.get('/registration/login', function(req, res, next) {
+  if (req.cookies.user) return res.redirect('/registration');
+  res.render('login');
+});
+
+function loginCheck(req, res, next) {
   res.cookie('user', req.body.username, util.cookieOptions.send);
   res.redirect("/");
-})
+}
+
+router.post('/petition/login', loginCheck);
+router.post('/registration/login', loginCheck);
 
 router.get('/logout', function (req, res, next) {
-  res.clearCookie('user');
+  res.clearCookie('user', util.cookieOptions.clear);
   res.redirect("/");
 });
 
 router.use("/petition", require('./petition'));
 // router.use("/registration", require('./registration'));
 
-// function getUserType(username, password, done) {
-//   setTimeout(function() {
-//     done('user', )
-//   });
-// }
-
 router.use(function (err, req, res, next) {
   if (err.message === "Unauthorized") {
     return res.render('error', {
-      message: 'Not logged in.', error: err
+      message: 'Invalid login information.', error: err
     });
   }
   next(err);
